518724d92a48eb4db83d27800d0661c390bed2ef
Nick Hawryluk <nick@entrobyte.com>
2021-08-22 09:44:03 -0400

    added simple UI Test Case for connected Outlets

--- GithubRecentsUITests/GithubRecentsUITests.swift
+++ GithubRecentsUITests/GithubRecentsUITests.swift
@@ -8,27 +8,19 @@
 import XCTest
 
 class GithubRecentsUITests: XCTestCase {
-
+    var app : XCUIApplication!
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app = XCUIApplication()
+        app.launch()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
     func testLaunchPerformance() throws {
@@ -38,5 +30,15 @@ class GithubRecentsUITests: XCTestCase {
                 XCUIApplication().launch()
             }
         }
+    }
+    
+    func testUserInputViewControllerInputFields(){
+        let app = XCUIApplication()
+        
+        app.textFields["GithubRecents"].tap()
+        app.textFields["100"].tap()
+        app.textFields["nick-havz"].tap()
+        
+        app/*@START_MENU_TOKEN@*/.staticTexts["FETCH!"]/*[[".buttons[\"FETCH!\"].staticTexts[\"FETCH!\"]",".staticTexts[\"FETCH!\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
     }
 }
